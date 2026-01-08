63acdd7b208bcf722f9506f5bd7f6c7133669264
Maria Morozova <59123858+mariamorozovaa@users.noreply.github.com>
2026-01-08 22:40:31 +0300

    Add FilterSidebar component

--- src/components/products/FilterSidebar.jsx
+++ src/components/products/FilterSidebar.jsx
@@ -1,3 +1,26 @@
-export default function FilterSidebar() {
-  return <div>FilterSidebar</div>;
+import { useGetCategoriesQuery } from "../../redux/api/productsApi";
+import Loader from "../common/Loader";
+
+export default function FilterSidebar({ onCategoryChange, selectedCategory }) {
+  const { data: categories, isLoading } = useGetCategoriesQuery();
+
+  if (isLoading) return <Loader />;
+
+  return (
+    <div>
+      <button onClick={() => onCategoryChange("all")} style={{ fontWeight: selectedCategory === "all" ? "bold" : "normal" }}>
+        Все
+      </button>
+      {categories.map((category) => (
+        <button
+          key={category}
+          style={{
+            fontWeight: selectedCategory === category ? "bold" : "normal",
+          }}
+          onClick={() => onCategoryChange(category)}>
+          {category}
+        </button>
+      ))}
+    </div>
+  );
 }
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,16 +1,39 @@
 import Loader from "../components/common/Loader";
 import ErrorMessage from "../components/common/ErrorMessage";
 import ProductGrid from "../components/products/ProductGrid";
-import { useGetProductsQuery } from "../redux/api/productsApi";
+import { useGetProductsQuery, useGetProductsByCategoryQuery } from "../redux/api/productsApi";
+import { useState } from "react";
+import FilterSidebar from "../components/products/FilterSidebar";
 
 export default function ProductsPage() {
-  const { data: products, isLoading, error } = useGetProductsQuery();
+  const [selectedCategory, setSelectedCategory] = useState("all");
+
+  const {
+    data: allProducts,
+    isLoading: isLoadingAll,
+    error: errorAll,
+  } = useGetProductsQuery(undefined, {
+    skip: selectedCategory !== "all",
+  });
+
+  const {
+    data: categoryProducts,
+    isLoading: isLoadingCategory,
+    error: errorCategory,
+  } = useGetProductsByCategoryQuery(selectedCategory, {
+    skip: selectedCategory === "all",
+  });
+
+  const products = selectedCategory === "all" ? allProducts : categoryProducts;
+  const isLoading = isLoadingAll || isLoadingCategory;
+  const error = errorAll || errorCategory;
 
   if (isLoading) return <Loader />;
   if (error) return <ErrorMessage message="Ошибка при загрузке продуктов" />;
 
   return (
     <div>
+      <FilterSidebar onCategoryChange={setSelectedCategory} selectedCategory={selectedCategory} />
       <ProductGrid products={products} />
     </div>
   );
